c074a9cadeb27f8967c3bda60b4765150e92ca67
toninikolaev0707-bit <toni.nikolaev.0707@mail.ru>
2026-03-29 17:56:57 +0300

    Update WeatherConfig.ts

--- src/types/WeatherConfig.ts
+++ src/types/WeatherConfig.ts
@@ -1,7 +1,7 @@
 import { Vector2 } from "@owlbear-rodeo/sdk";
 
 export interface WeatherConfig {
-  type: "SNOW" | "RAIN" | "SAND" | "FIRE" | "CLOUD" | "BLOOM"| "AURALINES"| "BLIZZARD";
+  type: "SNOW" | "RAIN" | "SAND" | "FIRE" | "CLOUD" | "BLOOM"| "AURALINES"| "BLIZZARD"| "LEAFSGREEN"| "LEAFSAUTUMN"| "LEAFSGOLDENPROMAX";
   speed?: number;
   direction?: Vector2;
   density?: number;
